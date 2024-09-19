21dce25c9cd0ce5eb86a74d417da9ce71c10deed
arturomrtn <moreton.arturo@gmail.com>
2024-09-19 15:52:27 +0200

    adding navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
-import { useState } from 'react'
-import './App.css'
+import React from 'react';
+import Navbar from './components/Navbar';
+import './App.css';
 
 function App() {
-
   return (
-<h1>Mi portfolio</h1>
-  )
+    <div className="App">
+      <Navbar />
+      <div className="content">
+        <section id="home">
+          <h1>Welcome to My Portfolio</h1>
+          <p>This is the home section.</p>
+        </section>
+        <section id="about">
+          <h1>About Me</h1>
+          <p>This is the about section.</p>
+        </section>
+        <section id="projects">
+          <h1>My Projects</h1>
+          <p>This is the projects section.</p>
+        </section>
+        <section id="contact">
+          <h1>Contact Me</h1>
+          <p>This is the contact section.</p>
+        </section>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
